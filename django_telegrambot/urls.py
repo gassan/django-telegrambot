612c3d3bfbd02326e2f34a6e397f5fc34b35c37e
autoantwort <41973254+autoantwort@users.noreply.github.com>
2023-12-20 11:17:54 +0100

    Update django.urls imports for newer Django (#25)
    Co-authored-by: Terrance <git@terrance.allofti.me>

--- django_telegrambot/urls.py
+++ django_telegrambot/urls.py
@@ -1,15 +1,17 @@
 
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 from django.conf import settings
 
-webhook_base = settings.DJANGO_TELEGRAMBOT.get('WEBHOOK_PREFIX','/')
+webhook_base = settings.DJANGO_TELEGRAMBOT.get('WEBHOOK_PREFIX', '/')
 if webhook_base.startswith("/"):
     webhook_base = webhook_base[1:]
 if not webhook_base.endswith("/"):
     webhook_base += "/"
 
 urlpatterns = [
-    url(r'admin/django-telegrambot/$', views.home, name='django-telegrambot'),
-    url(r'{}(?P<bot_token>.+?)/$'.format(webhook_base), views.webhook, name='webhook'),
+    re_path(r'admin/django-telegrambot/$',
+            views.home, name='django-telegrambot'),
+    re_path(r'{}(?P<bot_token>.+?)/$'.format(webhook_base),
+            views.webhook, name='webhook'),
 ]
